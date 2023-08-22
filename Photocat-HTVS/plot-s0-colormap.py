9ab54aa9cc8b9b536fa4b6dc203df24ac64102cd
Leticia Adao Gomes <100699955+adaogomesl@users.noreply.github.com>
2023-08-22 06:31:38 -0700

    Add files via upload

---
+++ Photocat-HTVS/plot-s0-colormap.py
@@ -0,0 +1,32 @@
+import sys,os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+title='%s' % sys.argv[1].split('.')[0]
+
+# Step 1: Read the data from the CSV file
+csv_name=title+'.csv'
+df = pd.read_csv(csv_name)
+
+# Step 2: Extract the data for the scatter plot
+xvalue='Oxi_'+title
+yvalue='Red_'+title
+x_values = df[xvalue]
+y_values = df[yvalue]
+z_values = df['Wavelength']  # Replace 'z_column_name' with the third column containing values for colormap
+
+# Step 3: Change the font settings
+font = {'family': 'sans-serif', 'weight': 'normal', 'size': 14}
+plt.rc('font', **font)
+#plt.figure(figsize=(8, 4))
+
+# Step 4: Create the scatter plot with colormap
+plt.scatter(x_values, y_values, c=z_values, cmap='viridis')
+plt.xlabel('Oxidation Potential (eV)')
+plt.ylabel('Reduction Potential (eV)')
+plt.colorbar(label='Wavelength (nm)')
+plt.grid(False)
+
+# Step 6: Save the plot as a PNG image
+plt.savefig('plot_'+title+'.png',transparent=True, bbox_inches='tight',dpi=850)
+
